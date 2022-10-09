aecf9ce9474411e50972536ba40fa43942b62982
Abhishek Lamichhane <amangrg3957@gmail.com>
2022-10-09 19:32:43 +0545

    New Update

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ $(document).ready(function(){
     });
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Programmer", "Developer", "Designer", "Freelancer"],
+        strings: ["Programmer", "Unity Developer", "Designer", "Freelancer", "Web Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
