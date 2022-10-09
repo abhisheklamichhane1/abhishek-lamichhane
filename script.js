f204cf19b610a4470dc62357ce8f302e617c16a6
Abhishek Lamichhane <amangrg3957@gmail.com>
2022-10-09 18:11:47 +0545

    Read More Btn

--- script.js
+++ script.js
@@ -43,6 +43,20 @@ $(document).ready(function(){
         loop: true
     });
 
+    //Read more button
+    const readMoreBtn = document.querySelector('.read-more-btn');
+    const text = document.querySelector('.text');
+
+    readMoreBtn.addEventListener('click', (e)=>{
+        text.classList.toggle('show-more');
+
+        if(readMoreBtn.innerText === 'Read More'){
+            readMoreBtn.innerText = 'Read Less';
+        }else{
+            readMoreBtn.innerText = 'Read More';
+        }
+    })
+
 
 
     // owl carousel script
