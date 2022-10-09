2a17675e9690811c1f21406cf80dd2a34f7b5fe2
Abhishek Lamichhane <amangrg3957@gmail.com>
2022-10-09 18:45:15 +0545

    Update

--- script.js
+++ script.js
@@ -45,12 +45,12 @@ $(document).ready(function(){
 
     //Read more button
     const readMoreBtn = document.querySelector('.read-more-btn');
-    const text = document.querySelector('.texti');
+    const text = document.querySelector('.text');
 
     readMoreBtn.addEventListener('click', (e)=>{
         text.classList.toggle('show-more');
 
-        if(readMoreBtn.innerText === 'Read More'){
+        if(readMoreBtn.innerText === 'Read more'){
             readMoreBtn.innerText = 'Read Less';
         }else{
             readMoreBtn.innerText = 'Read More';
